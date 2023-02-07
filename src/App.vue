ee3811e9fccf8aecac3dc0bf06c492ae9d575bee
yaroslavhorokhovets <gorohovecya@gmail.com>
2023-02-07 10:36:01 +0300

    first cvommit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <my-button size="small" @click="$emit('logout')" label="Search" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import MyButton from "./stories/Button.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    MyButton
+  }
+};
+</script>
+
+<style>
+@import "./index.css";
+</style>
