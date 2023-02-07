32aa65db7d614cc7be6172776e6edc1da4614fe6
yaroslavhorokhovets <gorohovecya@gmail.com>
2023-02-07 11:02:31 +0300

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <InboxScreen />
     <my-button size="small" @click="$emit('logout')" label="Search" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 import MyButton from "./stories/Button.vue";
-
+import InboxScreen from './components/InboxScreen.vue';
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    MyButton
+    // HelloWorld,
+    MyButton,
+    InboxScreen,
   }
 };
 </script>
---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,26 @@
+import { app } from '@storybook/vue3';
+import { createPinia } from 'pinia';
+app.use(createPinia());
+
+
+import PureInboxScreen from "./PureInboxScreen.vue";
+
+export default {
+  component: PureInboxScreen,
+  title: "PureInboxScreen",
+};
+
+const Template = (args) => ({
+  components: { PureInboxScreen },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: '<PureInboxScreen v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = { error: true };
